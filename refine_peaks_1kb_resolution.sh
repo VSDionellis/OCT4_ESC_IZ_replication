1ccc6e69afb5196fe3194dbaeca18ece88432d8c
vdionellis <52291098+VSDionellis@users.noreply.github.com>
2024-12-02 15:56:12 +0100

    refine_peaks_1kb_resolution.sh

--- refine_peaks_1kb_resolution.sh
+++ refine_peaks_1kb_resolution.sh
@@ -11,19 +11,20 @@ echo
 echo "Give the bigwig file of peak calling"
 read bw
 echo
+echo "Set number of threads"
+read threads
 echo 
-bedtools makewindows -b $peaks -w 1000 | bedtools intersect -a - -b $peaks -wa -wb  | awk -F '\t' 'BEGIN { OFS=FS } $7 != save { counter = 1; save = $7 } { print $0, counter++ }' | cut -f1-3,7,8 | sed 's/\(.*\)\t/\1_/' > $peaks.1kb_annotated.bed
+bedtools makewindows -b $peaks -w 1000 | bedtools intersect -a - -b $peaks -wa -wb  | awk -F '\t' 'BEGIN { OFS=FS } $7 != save { counter = 1; save = $7 } { print $0, counter++ }' | cut -f1-3,7,8 | sed 's/\(.*\)\t/\1_/' > ${peaks%.bed}.1kb_annotated.bed
 echo
 echo
-for f in *.1kb_annotated.bed
- do bigWigAverageOverBed $bw $f ${f%.bed.1kb_annotated.bed}.1kb_annotated.tab
-done
+ls *1kb_annotated.bed | sed "s/.bed//g" | xargs -P$threads -I{} sh -c "bigWigAverageOverBed $bw {}.bed {}.tab" -- {}
 echo
 echo
 for f in *.1kb_annotated.tab
- do paste ${f%.1kb_annotated.tab}.bed.1kb_annotated.bed $f | cut -f1-4,9 | sed "s/_/\t/g" | awk -v OFS="\t" '{print $4, $6, $0}' | sort -k1,1 -k2,2nr | awk -v OFS="\t" '!seen[$1]++ {print $3,$4,$5,$6,$8}' > ${f%.1kb_annotated.tab}.summits.bed
+ do paste ${f%.1kb_annotated.tab}.1kb_annotated.bed $f | cut -f1-4,9 | sed "s/_/\t/g" | awk -v OFS="\t" '$6>max[$4]{max[$4]=$6; row[$4]=$0} END{for (i in row) print row[i]}' > ${f%.1kb_annotated.tab}.summits.bed
 done
 echo
+rm *.1kb_annotated.tab *.1kb_annotated.bed
 echo "DONE!!!"
 echo
 exit
